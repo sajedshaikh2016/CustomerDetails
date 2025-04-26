51ab4019823afb5b833869612129af8cf4daaf88
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2025-04-26 10:47:28 +0530

    Initial Commit

---
+++ CustomerDetails/CustomerDetailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomerDetailsApp.swift
+//  CustomerDetails
+//
+//  Created by Sajed Shaikh on 26/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomerDetailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CustomerDetailsTests/CustomerDetailsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CustomerDetailsTests.swift
+//  CustomerDetailsTests
+//
+//  Created by Sajed Shaikh on 26/04/25.
+//
+
+import Testing
+@testable import CustomerDetails
+
+struct CustomerDetailsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
